84166888244787747be6729fa05645d0b36b193c
Abbas <meerabbas110@gmail.com>
2023-06-07 00:18:10 +0530

    pratices the date api

---
+++ 2.variables/date-time.js
@@ -0,0 +1,38 @@
+//let d;
+
+//d = new Date();
+// d= d.toString();
+//in date months are zero based or zero index so
+
+// d = new Date(2021,0,12, 11,30,36)
+//passing the value of date in striing will hep to start the months from 1 index
+// d = new Date('2021,5,12')
+
+// d = new Date(1686051906674);
+
+//d = Math.floor(Data.now()/1000)
+//console.log(d);
+
+// let x 
+// let d = new Date()
+
+// console.log(object);
+
+let d;
+
+x = d.toLocaleString('default', {month: 'short'})
+
+console.log(x);
+
+let date;
+
+x = date.toLocaleString('default', {
+    year: 'long',
+    month: 'numaric',
+    days: 'numaric',
+    hour: 'numeric',
+    minute: 'numeric',
+    secods: 'numeric',
+    timeZone: 'america/new_yourk'
+
+})
