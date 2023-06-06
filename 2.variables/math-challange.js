d9bb9212a72b404a5654f3c09795bb6e599ddb19
Abbas <meerabbas110@gmail.com>
2023-06-06 10:49:08 +0530

    challange done

---
+++ 2.variables/math-challange.js
@@ -0,0 +1,20 @@
+let x;
+let y;
+
+x = Math.floor(Math.random()*100 +1)
+
+y = Math.floor(Math.random()*50 +1)
+
+let addi = `add of ${x} ${y} is = ${x+y}`;
+console.log(addi);
+
+let div = `division of ${x} ${y} is = ${x/y}`;
+console.log(div);
+
+
+let modu = `modueles of ${x} ${y} is = ${x%y}`;
+console.log(modu);
+
+const multi = x * y;
+const multioutput = `${x} * ${y} = ${multi}`;
+console.log(multioutput);
