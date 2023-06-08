897ba762b5fd564849c90f6a00cfe9a7e1d9dea5
Abbas <meerabbas110@gmail.com>
2023-06-08 11:12:32 +0530

    challange on array methods solved

---
+++ 3.arrays&obj/challange.js
@@ -0,0 +1,23 @@
+let x;
+let arr = [1,2,3,4,5]
+x= arr.reverse().unshift(6).push(0)
+x = arr
+x= arr.
+
+//console.log(x,arr);
+
+const arr1 = [1,2,3,4,5]
+const arr2 = [5,6,7,8,9,10]
+
+const arr3= [...arr1, ...arr2]
+
+arr3.slice(1,5)
+console.log(arr3);
+
+//to use a spread operator we have to declear [] this brackets
+//slice just return us the copy of array that won't affcet the real array and it take the argument as (1st index 4and value before this index)
+//splice return us the same array modified and it use to take args as (1start from 7end at)
+
+
+
+
