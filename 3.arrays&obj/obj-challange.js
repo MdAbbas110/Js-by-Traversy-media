25a2f3663f9989e811ee70d669f912a16e3ba6a4
Abbas <meerabbas110@gmail.com>
2023-06-10 14:32:03 +0530

    all the steps are done

--- 3.arrays&obj/obj-challange.js
+++ 3.arrays&obj/obj-challange.js
@@ -1,5 +1,6 @@
+// step 1
 const library = [
-    {Title: 'Quran',
+    {title: 'Quran',
     author: 'Allah',
     status: {
         own: true,
@@ -17,7 +18,7 @@ const library = [
         }
     },
     {
-        title: 'Nahjul Balagah',
+        title: 'Nahjul Balagah 2',
         author: 'Imam Ali',
         status:{
             own: true,
@@ -27,7 +28,16 @@ const library = [
     }
 ]
 
+// step 2//
 let x = library[0].status.read = true;
 let x1 = library[1].status.read = true;
 let x2 = library[2].status.read = true;
-console.log(x,x1,x2);
+//console.log(x,x1,x2)
+
+const {title: FirstBook } = library[0]
+//console.log(FirstBook);
+
+// step 4
+
+const libraryJSON = JSON.stringify(library);
+console.log(libraryJSON);
