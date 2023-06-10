16a225a9acbc5696c185eefe764808067860d193
Abbas <meerabbas110@gmail.com>
2023-06-11 00:36:20 +0530

    updated the change

---
+++ 3.arrays&obj/funChallange.js
@@ -0,0 +1,10 @@
+// function getCelsius (f){
+    // const celsius = (f -32) *5 /9
+    // return celsius
+// }
+
+// console.log(getCelsius(50));
+
+const getCel = f => (f - 32)*5/9;
+
+console.log(`The celsics is ${getCel(99)} \xB0C`);
