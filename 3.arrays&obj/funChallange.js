bb15e610a431afdce6be712bbe56a44cbbccd6cc
Abbas <meerabbas110@gmail.com>
2023-06-11 14:57:11 +0530

    min max with Math function

--- 3.arrays&obj/funChallange.js
+++ 3.arrays&obj/funChallange.js
@@ -5,6 +5,20 @@
 
 // console.log(getCelsius(50));
 
-const getCel = f => (f - 32)*5/9;
+//const getCel = f => (f - 32)*5/9;
 
-console.log(`The celsics is ${getCel(99)} \xB0C`);
+//console.log(`The celsics is ${getCel(99)} \xB0C`);
+
+//challange 2
+
+function minMax (arr){
+    const min = Math.min(...arr)
+    const max = Math.max(...arr)
+    return {
+        min,
+        max
+    }
+}
+
+let array = minMax([1,23,4,56,7,60,32])
+console.log(array);
