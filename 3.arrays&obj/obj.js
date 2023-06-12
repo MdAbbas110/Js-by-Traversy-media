daa9eb5cdfc97e506ec84db91cfbece19fd8ab8e
Abbas <meerabbas110@gmail.com>
2023-06-12 23:37:46 +0530

    good graps on ternary operator

---
+++ 3.arrays&obj/obj.js
@@ -0,0 +1,15 @@
+//there is a object littral that is object how it use to be declear
+
+// const obj{
+    // one: 'name'
+// }
+
+//ternary operators
+//there are basically two ways to assign the one
+
+const te = arr >= 8 ? (console.log('here is it'), '/saved to var') : (console.log('here else'), '/saved to variable') 
+
+//logical operators in ternary
+
+
+
