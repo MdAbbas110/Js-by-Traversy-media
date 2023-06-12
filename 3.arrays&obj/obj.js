e4293a90f7cd9073b76ecfc24fff8654c36468cf
Abbas <meerabbas110@gmail.com>
2023-06-12 23:41:24 +0530

    nullish colustionn gettting good graps

--- 3.arrays&obj/obj.js
+++ 3.arrays&obj/obj.js
@@ -11,5 +11,12 @@ const te = arr >= 8 ? (console.log('here is it'), '/saved to var') : (console.lo
 
 //logical operators in ternary
 
+a = false
 
+a ||= 23 // * if falsy values will encounter it will always print the right side value wont print the false 0 on output
 
+//for undefined and null value to encounter and over cum to that ?? we use nullish colluton'
+
+let b = null 
+
+b ??= 110 // ?? it will alwasy print the right value in case of null or un but easly can print false and 0
