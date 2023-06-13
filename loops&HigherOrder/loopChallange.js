18b0522a26da8215bc6a09e803dd66f3da6e7063
Abbas <meerabbas110@gmail.com>
2023-06-13 10:55:22 +0530

    Fizz Buzz challange in regular for loop and if

---
+++ loops&HigherOrder/loopChallange.js
@@ -0,0 +1,17 @@
+// Most comman FizzBuzz Challange
+
+for(let i =1; i <= 100; i++){
+    if(i % 3 ===0 && i % 5 === 0){
+        console.log('FizzBuzZ');
+    }
+    else if(i % 3 === 0){
+        console.log('Fizz');
+    }else if (i % 5 === 0){
+        console.log('Buzz');
+    }else{
+    console.log(i);
+}
+}
+
+// fizz Buzz Using while loop and Ternary operators
+
