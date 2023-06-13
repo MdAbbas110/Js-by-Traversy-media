128c15ec6d748419e0fb99df77dbe7191b3faab7
Abbas <meerabbas110@gmail.com>
2023-06-13 11:10:52 +0530

    While loop and ternaray operator FizzBuzz

--- loops&HigherOrder/loopChallange.js
+++ loops&HigherOrder/loopChallange.js
@@ -1,5 +1,5 @@
 // Most comman FizzBuzz Challange
-
+/*
 for(let i =1; i <= 100; i++){
     if(i % 3 ===0 && i % 5 === 0){
         console.log('FizzBuzZ');
@@ -12,6 +12,14 @@ for(let i =1; i <= 100; i++){
     console.log(i);
 }
 }
-
+*/
 // fizz Buzz Using while loop and Ternary operators
 
+let i = 1
+
+while( i <= 100){
+    i% 3 ===0 && i%5 ===0 ? console.log('FizzBuzz')
+    : i%3 === 0 ? console.log('fizz') 
+    : i % 5 === 0 ? console.log('Buzzz') : console.log(i);
+    i++;
+}
