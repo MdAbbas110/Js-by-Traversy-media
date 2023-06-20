9e64af1331da41f644a7af8fce684b909351c1bf
Abbas <meerabbas110@gmail.com>
2023-06-21 00:34:36 +0530

    learning about the promise and handson

---
+++ Aysncroness-js/promises/script.js
@@ -0,0 +1,45 @@
+// *promise are objects that represents the completion or the failor  of the asscrnoness operation
+
+//? Mostly we gonna be using the promise to handele the resond from the api or json file
+
+//To create a promise we use cunstroctor function of new keyworld
+
+ const promise = new Promise((resolve,reject)=>{
+//promise take 2 things in its parameter one is resolve and reject if the task sussefully done it exicute the resolve eles reject
+//? we offenly use settime out with the promise to get the data from the server coz it takes time
+
+//do some async take
+setTimeout(()=>{
+    console.log('async task complete');
+    resolve()
+    },1000)
+ })
+
+ //* how to call the promise is by using the variable that is assigned and .then() then will aslo take a call back function
+
+ promise.then(()=>{//anything that we pass in to resolve
+    //console.log('promise exicuted');
+ })
+
+ //? most offen you will se that .then() method use to put directoly on the promsie innitalized part only
+
+ getUser = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let error = true
+
+        if (!error) {
+            resolve({name:'abbas', age: 24})
+        } else {
+            reject('something went wrong')
+        }
+
+        },1000)
+     })
+     
+     //.then() will handel the resolve
+getUser
+    .then((user)=>console.log(user))
+    .catch((error) => console.log(error))//catch() use to handle error
+    .finally(() => console.log('promise is running force fully'))
+// finally is use to pass to exicute the promise any way either resolve or rejected
+ 
