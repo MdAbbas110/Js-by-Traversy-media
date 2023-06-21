fbad4537de5fd07182ce1be7b23307987febc913
Abbas <meerabbas110@gmail.com>
2023-06-21 22:06:04 +0530

    complt the promise part nd diff ways to handle it

--- Aysncroness-js/promises/NoCallBackHell.js
+++ Aysncroness-js/promises/NoCallBackHell.js
@@ -19,18 +19,38 @@ function getData(endpoint) {
 }
 //to call the promise function that we decleard promise on first we need to call the functon() and in the argument of fun we declear the endPoint of the request url and after that we call the .then with cb
 
-getData('./movie.json')
-.then((movies) =>{
-    console.log(movies);
-    return getData('./actor.json')
+
+//* this is a way of doing the thing using then chainning bi there is a much more clean way we can do the same using all
+
+// getData('./movie.json')
+// .then((movies) =>{
+    // console.log(movies);
+    // return getData('./actor.json')
+// })
+// .then((actor)=>{
+    // console.log(actor);
+    // return getData('./director.json')
+// })
+// .then((director)=>{
+    // console.log(director);
+// })
+// .catch((error)=>{
+    // alert(error);
+// })
+
+//? here is an other way of channing the apis call if we have multiple using promise.all
+
+const moviePromises = getData('./movie.json')
+const actorPromises = getData('./actor.json')
+const directorPromises = getData('./director.json')
+
+const dummy = new Promise((resolve, reject)=>{
+    resolve({abbas: 'Abidi', status:'wilayati'})
 })
-.then((actor)=>{
-    console.log(actor);
-    return getData('./director.json')
+
+
+Promise.all([moviePromises, dummy, actorPromises, directorPromises])
+.then((data) =>{// this will bring the arrays of data kyu ke balti ek hai last time 3 alag balti the to alag mil raha tha ab ek hai to usi me sab mil raha hai but ek ke upper ek 
+console.log(data);
 })
-.then((director)=>{
-    console.log(director);
-})
-.catch((error)=>{
-    alert(error);
-})
+.catch(error => console.log(error))
