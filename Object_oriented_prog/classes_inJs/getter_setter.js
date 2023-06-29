e309f3f6bf66a6eef8233445b5bdb587ebce2d13
Abbas <meerabbas110@gmail.com>
2023-06-29 18:29:21 +0530

    confirigable, emeurable, seaded,fixed, writeable

--- Object_oriented_prog/classes_inJs/getter_setter.js
+++ Object_oriented_prog/classes_inJs/getter_setter.js
@@ -77,11 +77,74 @@ wallet.withdraw(35);
 wallet.withdraw(20);
 wallet.deposit(250);
 wallet.withdraw(80);
-console.log(wallet.balance);
-console.log(wallet.transictions);
+//console.log(wallet.balance);
+//console.log(wallet.transictions);
+
 // here is the good example of encapculation we have the this.balance property but we are not using it
 //out side of the class we have created the get method to acess that properties
 
 //* up until this time we were just using the _name to indicate the properety as a private property 
 //* but in ES2022 we got the way to acutlly prevent the private property to acess from out side the calss that is a number sign #
-//* previwsly we can aceess them but with this function we can prevent the class to aceess
+//* previwsly we can aceess them but with this function we can prevent the class to aceess similarly like java private class'
+
+class Wallet2 {
+//to use it in clss we dont need constructor we can directy call propertie name with #
+    #balance = 0;
+    #transictions = [];
+
+   deposit(amount) {
+    this.#processDeposit(amount)
+    this.#balance += amount
+    }
+    #processDeposit(amount) {
+        console.log(`Depositing Amount ${amount}`);
+        this.#transictions.push({
+            type: 'deposit',
+            amount
+        })
+    }
+
+    withdraw(amount) {
+        if (amount > this.#balance) {
+            console.log('not enough fund to withdraw');
+            return
+        }
+        this.#processWithDraw(amount)
+        this.#balance -= amount
+    }
+    #processWithDraw(amount) {
+        console.log(`withdrawing Amount ${amount}`);
+        this.#transictions.push({
+            type: 'withdraw',
+            amount
+        })
+    }
+//when we set a getter method we can acess it like a prope
+    get balance () {
+        return this.#balance
+        //created a getter and passed the #namee there is no error for that and now we can acess it via getter
+    }
+    get transictions() {
+        return this.#transictions
+    }
+}
+
+const wallet2 = new Wallet2()
+//console.log(wallet2.balance);
+//as we cant add the property and acess it out of the class we need to pass it in a getter
+//wallet2.balance = '123'
+
+const rect = {
+    name: 'abbas',
+    height: 5.1,
+    cast: 'shia'
+}
+
+Object.defineProperty(rect, 'name', {
+    writable: false,
+    configurable: false,
+    enumerable: false
+}) 
+
+let prop = Object.getOwnPropertyDescriptors(rect, 'name')
+console.log(prop);
